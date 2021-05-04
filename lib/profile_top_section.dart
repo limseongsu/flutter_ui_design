38e24998e4df3b227288027016bf97b5c5504d46
imseongsu <tjdtn2323@naver.com>
2021-05-04 12:28:37 +0900

    image를 가져와 캐로셀 슬라이더(라이브러리)를 통해 슬라이더 형식을 만들어주고, 포지션을 통해 현재 image의 index를 확인할 수 있게 밑에 바를 만들어 주었다.

--- lib/profile_top_section.dart
+++ lib/profile_top_section.dart
@@ -2,28 +2,66 @@ import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_ui_design/models/profile.dart';
 
-class ProfileTopSection extends StatelessWidget {
+class ProfileTopSection extends StatefulWidget {
   final Profile profile;
 
   ProfileTopSection(this.profile);
 
   @override
+  _ProfileTopSectionState createState() => _ProfileTopSectionState();
+}
+
+class _ProfileTopSectionState extends State<ProfileTopSection> {
+  int _index;
+
+  @override
   Widget build(BuildContext context) {
-    return CarouselSlider(
-        items: profile.profileImages
-      .map((e) => Image.network(
-          e,
-          width: MediaQuery.of(context).size.width,
-          height: 370.0,
-        fit: BoxFit.cover,
-        )).toList(),
-      options: CarouselOptions(
-        height: 370.0,
-        //사진을 width 꽉 차게
-        viewportFraction: 1,
-        //인피니트스크롤을 막는것
-        enableInfiniteScroll: false,
-      ),
+    return Stack(
+      children: [
+        CarouselSlider(
+          items: widget.profile.profileImages
+              .map((e) => Image.network(
+                    e,
+                    width: MediaQuery.of(context).size.width,
+                    height: 370.0,
+                    fit: BoxFit.cover,
+                  ))
+              .toList(),
+          options: CarouselOptions(
+            height: 370.0,
+            //사진을 width 꽉 차게
+            viewportFraction: 1,
+            //인피니트스크롤을 막는것
+            enableInfiniteScroll: true,
+            autoPlay: true,
+            onPageChanged: (index, reason) {
+              setState(() {
+                _index = index;
+              });
+            },
+          ),
+        ),
+        Positioned(
+          bottom: 30,
+          left: 0,
+          right: 0,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: widget.profile.profileImages.asMap().entries.map((e) {
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 10,
+                  height: 10,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: e.key == _index ? Colors.red : Colors.white),
+                ),
+              );
+            }).toList(),
+          ),
+        ),
+      ],
     );
   }
 }
