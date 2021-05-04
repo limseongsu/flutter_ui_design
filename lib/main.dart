c7d954d8555e8a7b613719cdc717951e19dbf41b
imseongsu <tjdtn2323@naver.com>
2021-05-04 12:41:03 +0900

    image를 가져와 캐로셀 슬라이더(라이브러리)를 통해 슬라이더 형식을 만들어주고, 포지션을 통해 현재 image의 index를 확인할 수 있게 밑에 바를 만들어 주었다. main page의 하단 버튼영역을 나누어 주었다.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
-
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_design/data/fake_data.dart';
 import 'package:flutter_ui_design/models/profile.dart';
 import 'package:flutter_ui_design/profile_top_section.dart';
+
 void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget {
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,16 +25,19 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   final Profile profile = fakeprofile;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 67,
-        leading: Icon(Icons.arrow_back_ios,
+        leading: Icon(
+          Icons.arrow_back_ios,
           color: Colors.black,
         ),
-        title: Text('프로필'
-          ,style: TextStyle(color: Colors.black),
+        title: Text(
+          '프로필',
+          style: TextStyle(color: Colors.black),
         ),
         centerTitle: false,
         titleSpacing: 0.0,
@@ -47,10 +50,33 @@ class MyHomePage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          ProfileTopSection(profile),
+          Expanded(
+            child: ListView(
+              children: [
+                ProfileTopSection(profile),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: ElevatedButton(
+                  onPressed: null,
+                  child: Text('별로에요'),
+                ),
+              ),
+              Expanded(
+                flex: 1,
+                child: ElevatedButton(
+                  onPressed: null,
+                  child: Text('괜찮아요'),
+                ),
+              ),
+            ],
+          )
         ],
       ),
     );
   }
 }
-
